ab9430444276bedcad8a26824dbdbc44846fa9d5
kikx0319 <karloestanislao@yahoo.com>
2020-07-01 22:15:32 -0700

    completed

--- server/index.js
+++ server/index.js
@@ -1,19 +1,20 @@
-require('dotenv').config()
-const express = require('express');
-const massive = require('massive');
-const products_controller = require('./products_controller')
-
+require("dotenv").config();
+const express = require("express");
+const massive = require("massive");
+const products_controller = require("./products_controller");
 
 const app = express();
+
 const { SERVER_PORT, CONNECTION_STRING } = process.env;
 
 massive({
-    connectionString: CONNECTION_STRING,
-    ssl: { rejectUnauthorized: false }
-}).then(dbInstance => {
-    app.set('db', dbInstance);
+connectionString: CONNECTION_STRING,
+ssl: {rejectUnauthorized: false}
 })
-    .catch(err => console.log(err));
+  .then(dbInstance => {
+    app.set("db", dbInstance);
+  })
+  .catch(err => console.log(err));
 
 app.use(express.json());
 
@@ -23,9 +24,6 @@ app.get('/api/products/:id', products_controller.getOne);
 app.put('/api/products/:id', products_controller.update);
 app.delete('/api/products/:id', products_controller.delete);
 
-
 app.listen(SERVER_PORT, () => {
-    console.log(`Server running on ${SERVER_PORT}.`);
+  console.log(`Server listening on port ${SERVER_PORT}.`);
 });
-
-
